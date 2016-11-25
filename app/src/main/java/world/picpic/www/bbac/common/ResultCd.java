608a2e74d022b5e53ab47149305c9bc3bd2035e1
Wonseob <kws0210@naver.com>
2016-11-25 10:08:40 +0900

    added files

---
+++ app/src/main/java/world/picpic/www/bbac/common/ResultCd.java
@@ -0,0 +1,12 @@
+package world.picpic.www.bbac.common;
+
+/**
+ * Created by Wonseob on 2016. 11. 23..
+ */
+
+public class ResultCd {
+    public static String SUCCESS = "0";
+    public static String SUCCESS_WITH_ALERT = "1";
+    public static String SUCCESS_WITH_DIALOG = "2";
+    public static String FAILURE = "4";
+}
